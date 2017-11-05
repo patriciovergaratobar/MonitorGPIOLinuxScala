bef98df8be8a981c771e0dde198d7b2e62b6735f
gaston <gaston.lucerom@gmail.com>
2017-11-05 16:42:15 +0100

    Changes in messages

--- MonitorGPIOLinuxScala/src/com/pvergara/scala/monitorGPIOLinux/ExampleGPIOIn.scala
+++ MonitorGPIOLinuxScala/src/com/pvergara/scala/monitorGPIOLinux/ExampleGPIOIn.scala
@@ -12,11 +12,11 @@ object ExampleGPIOIn {
   def main(args: Array[String]): Unit = {
 
     val monitorGPIO = new MonitorGPIO()
-    println("Star/ting MonitorGPIO, every 10 seconds will evaluate gpio`s states (implicit value)")
+    println("Starting MonitorGPIO, every 10 seconds will evaluate gpio`s states (implicit value)")
     val gpio57 = Gpio(GpioUtil.GPIO_57, Off(false))
     val gpio82 = Gpio(GpioUtil.GPIO_82, Off(false))
-    println(s"Gpio $gpio57 ready")
-    println(s"Gpio MonitorGPIO $gpio82 ready")
+    println(s"MonitorGPIO $gpio57 ready")
+    println(s"MonitorGPIO $gpio82 ready")
     monitorGPIO.startMonitor(List(gpio57, gpio82))
   }
 

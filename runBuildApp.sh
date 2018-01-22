dd4e4e6c57744bb8eadf5d9d32baad4d28d5952c
patriciovergaratobar <patricio.vergara.tobar@gmail.com>
2018-01-21 21:45:58 -0300

    update sh

--- runBuildApp.sh
+++ runBuildApp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 echo "========================================================"
 echo "COMPILANDO MONITOR GPIOS"
 echo "========================================================"
-
 #Se Importa la configuracion
 rutaLocal=$(realpath -s $0 | awk -F${0##*/} '{print $FNR}')
 cd $rutaLocal
@@ -11,7 +9,6 @@ echo "Ejecutando sbt assembly"
 sbt assembly
 estadoCompilacion=$?
 echo "El estado de la compilación es $estadoCompilacion"
-
 echo "Copiando jar compilado en packagedApp/"
 cp target/scala-2.12/MonitorGPIOLinuxScala-assembly-1.0.jar packagedApp/
 
